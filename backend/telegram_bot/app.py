64a2041cb8d660c5f81d79bc41930e7e1e6bb23f
Prince Jonathan Nkansah <jonathannkansah13@gmail.com>
2022-05-10 07:35:05 +0000

    heroku integration

--- backend/telegram_bot/app.py
+++ backend/telegram_bot/app.py
@@ -15,7 +15,8 @@ APP = Flask(__name__)
 cors=CORS(APP)
 APP.config['CORS_HEADERS'] = ["Content-Type"]
 
-APP.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Jpn@pg13@localhost:5432/ttmg'
+# APP.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Jpn@pg13@localhost:5432/ttmg'
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rxxavqzelrynsk:8aec395e670af85437c5c603f01b4c8f608ed7eb2c86e568d2864e53cb1dc3a8@ec2-44-196-223-128.compute-1.amazonaws.com:5432/d96dk3m2laba85'
 # consumes lot of memory: set to false
 APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 APP.config['SQLALCHEMY_ECHO'] = True
